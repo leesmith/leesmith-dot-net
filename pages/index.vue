f950c0bf9ecc7d6e5346f63465a271127be96cfa
Lee Smith <jeremyleesmith@gmail.com>
2018-06-10 15:21:32 -0500

    Add aside content

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,17 @@
         <fa-icon :icon="['far', 'calendar-alt']" /> June 3, 2018
         <a href="#" class="is-size-3 is-block has-text-weight-semibold">New site who dis</a>
       </div>
-      <div class="column">
-        <p><nuxt-link to="/about">About</nuxt-link></p>
-      </div>
+      <aside class="column">
+        <div class="headshot"></div>
+        <ul class="aside-list">
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/about">About</nuxt-link></li>
+          <li>
+            <a class="is-size-4" href="https://www.twitter.com/jeremyleesmith" target="_blank" style="margin-right: 16px; color: #1da1f2;"><fa-icon :icon="['fab', 'twitter']" /></a>
+            <a class="is-size-4" href="https://www.github.com/leesmith" target="_blank" style="color: #24292e;"><fa-icon :icon="['fab', 'github']" /></a>
+          </li>
+        </ul>
+      </aside>
     </div>
   </div>
 </template>
