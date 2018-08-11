295b3f074731064a5e4ec73fe22a845188c15b2b
Lee Smith <jeremyleesmith@gmail.com>
2018-08-11 15:02:30 -0500

    Add responsive css classes

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <ul class="post-list">
       <li>
         <div class="is-block"><fa-icon :icon="['far', 'calendar-alt']" /> June 3, 2018</div>
-        <nuxt-link to="/posts/new-site" class="is-size-3 has-text-weight-semibold fancy-underline">New site who dis</nuxt-link>
+        <nuxt-link to="/posts/new-site" class="is-size-3-desktop is-size-4-tablet is-size-5-mobile has-text-weight-semibold fancy-underline">
+          New site who dis
+        </nuxt-link>
       </li>
     </ul>
   </div>
